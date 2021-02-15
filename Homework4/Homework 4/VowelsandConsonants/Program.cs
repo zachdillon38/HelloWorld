a47b6651b6c4ca2819a332e038399cdb75fb87a4
zachdillon38 <70294223+zachdillon38@users.noreply.github.com>
2021-02-14 22:16:10 -0600

    Add files via upload

---
+++ Homework4/Homework 4/VowelsandConsonants/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelsandConsonants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zach Dillon
+
+            //Declare variables to hold counting of vowels and consonants
+
+            int Consonants = 0;
+            int Vowels = 0;
+            int i;
+
+            //Ask for the user to input the sentence
+
+            Console.WriteLine("Please enter a sentence...");
+            String input = Console.ReadLine().ToLower();
+
+            //Declare integer length to count the length of the sentence
+
+            int Length = input.Length;
+
+
+
+            //Begin For statement to count the length
+
+
+            for (i = 0; i < Length; i++)
+            {
+                //Declare character value to identify with input
+
+                Char x = input[i];
+
+                if (Char.IsLetter(x))
+                {
+                    if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
+                    {
+                        Vowels++;
+
+                    }
+
+
+                    else if (x != 'a' || x != 'e' || x != 'i' || x != 'o' || x != 'u')
+                    {
+                        Consonants++;
+                    }
+                }
+            }
+            Console.WriteLine("There were " + Vowels + " vowels in your sentence.");
+            Console.WriteLine("There were " + Consonants + " consonants in your sentence");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Homework4/Homework 4/l337Converter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l337Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zach Dillon
+
+            //Prompt for user input, convert to upper
+
+            Console.WriteLine("Please enter a sentence...");
+            String sentence = Console.ReadLine().ToUpper();
+
+            //Perform replacements
+
+            sentence = sentence.Replace("A", "@").Replace("E", "3").Replace("H", "|-|").Replace("S", "$").Replace("T", "7").Replace("U", "|_|");
+
+            //Insert ! if necessary
+
+            if (sentence.EndsWith("!"))
+            {
+                Console.WriteLine(sentence);
+
+            }
+            else
+
+            {
+                Console.WriteLine(sentence + "!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
